889fce3a1da0a95aa5bd1684a206e9473274bd78
Bruno Lima <93183810+Brunoo-Lima@users.noreply.github.com>
2024-02-12 14:07:40 -0300

    feat: remove product from cart

--- src/redux/cart/actions.js
+++ src/redux/cart/actions.js
@@ -4,3 +4,8 @@ export const addProductToCart = (payload) => ({
   type: cartActionTypes.ADD_PRODUCT,
   payload,
 });
+
+export const removeProductFromCart = (payload) => ({
+  type: cartActionTypes.REMOVE_PRODUCT,
+  payload,
+});
--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -30,6 +30,14 @@ const cartReducer = (state = initialState, action) => {
         products: [...state.products, { ...action.payload, quantity: 1 }],
       };
 
+    case cartActionTypes.REMOVE_PRODUCT:
+      return {
+        ...state,
+        products: state.products.filter(
+          (product) => product.id !== action.payload,
+        ),
+      };
+
     default:
       return state;
   }
